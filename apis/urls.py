93b60a53de006112ee527abff209bf25bc82b3c3
Nithyasri Babu <nithyasri.babu@gmail.com>
2020-02-23 01:46:58 -0500

    Json Responce for Get all values from tables

--- apis/urls.py
+++ apis/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from apis import views
 
 urlpatterns = [
-    path('products/', views.ProductsListCreate.as_view()),
-    path('households/', views.HouseholdsListCreate.as_view()),
-    path('transactions/', views.TransactionsListCreate.as_view()),
+    path('products/', views.all_products,name='all_products'),
+    path('households/', views.all_households,name='all_households'),
+    path('transactions/', views.all_transactions,name='all_transactions'),
 ]
--- apis/views.py
+++ apis/views.py
@@ -1,20 +1,18 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, JsonResponse
 from apis.models import Products, Households, Transactions
-from apis.serializers import ProductsSerializer, HouseholdsSerializer, TransactionsSerializer
-from rest_framework import generics
 
-class ProductsListCreate(generics.ListCreateAPIView):
-    queryset = Products.objects.all()
-    serializer_class = ProductsSerializer
+def all_products(request):
+    products = Products.objects.all()
+    results = {'results': list(products.values('product_id', 'department', 'commodity', 'brand_type','organic'))}
+    return JsonResponse(results)
 
-class HouseholdsListCreate(generics.ListCreateAPIView):
-    queryset = Households.objects.all()
-    serializer_class = HouseholdsSerializer
+def all_households(request):
+    households = Households.objects.all()
+    results = {'results': list(households.values('hshd_id', 'loyalty','age_range', 'marital_status','income_range','homeowner','hshd_comp','hh_size','children'))}
+    return JsonResponse(results)
 
-class TransactionsListCreate(generics.ListCreateAPIView):
-    queryset = Transactions.objects.all()
-    serializer_class = TransactionsSerializer
-
- 
+def all_transactions(request):
+    transactions = Transactions.objects.all()
+    results = {'results': list(transactions.values('hshd_id', 'bskt_id', 'trans_date', 'product_id', 'spend','units','store_region', 'week_num', 'year'))}
+    return JsonResponse(results)
